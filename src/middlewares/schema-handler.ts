cdf75de26cbc0eb03d81e462c28f127750411076
Matheus Henrique <matthpmoreira@gmail.com>
2025-01-07 14:14:35 -0300

    Refactor HTTP status in `src/middlewares/schema-handler.ts`

--- src/middlewares/schema-handler.ts
+++ src/middlewares/schema-handler.ts
@@ -1,11 +1,12 @@
 import { NextFunction, Request, Response } from "express";
 import { ObjectSchema } from "joi";
+import httpStatus from "http-status";
 
 export function validateSchemaMiddleware(schema: ObjectSchema) {
   return (req: Request, res: Response, next: NextFunction) => {
     const validation = schema.validate(req.body, { abortEarly: false });
     if (validation.error) {
-      return res.status(422).send({ error: validation.error.message });
+      return res.status(httpStatus.UNPROCESSABLE_ENTITY).send({ error: validation.error.message });
     }
 
     next();
